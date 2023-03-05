f18ce6b650a7c8f75d03c88c7b8a63b343cdc8d4
Kyle Wilson <wilson15701@gmail.com>
2023-03-04 22:34:39 -0500

    add more unit tests

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -132,3 +132,145 @@ metadata:
 		})
 	}
 }
+
+func TestParser_YamlFile(t *testing.T) {
+	type fields struct {
+		Resources []Resource
+	}
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{
+			name: "yaml file name",
+			args: args{
+				name: "my-FILE",
+			},
+			want: "my-file.yaml",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Parser{
+				Resources: tt.fields.Resources,
+			}
+			if got := p.YamlFile(tt.args.name); got != tt.want {
+				t.Errorf("Parser.YamlFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParser_kustomizeResourcePaths(t *testing.T) {
+	type fields struct {
+		Resources []Resource
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []string
+	}{
+		{
+			name: "kustomize resource paths",
+			fields: fields{
+				Resources: []Resource{
+					{
+						"kind": "Kustomization",
+					},
+					{
+						"kind": "Service",
+					},
+					{
+						"kind": "Service",
+					},
+					{
+						"kind": "Deployment",
+					},
+				},
+			},
+			want: []string{"deployment.yaml", "service.yaml"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Parser{
+				Resources: tt.fields.Resources,
+			}
+			if got := p.kustomizeResourcePaths(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Parser.kustomizeResourcePaths() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParser_Kustomize(t *testing.T) {
+	type fields struct {
+		Resources []Resource
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   Resource
+	}{
+		{
+			name: "kustomize",
+			fields: fields{
+				Resources: []Resource{
+					{
+						"kind": "Kustomization",
+					},
+					{
+						"kind": "Service",
+					},
+					{
+						"kind": "Service",
+					},
+					{
+						"kind": "Deployment",
+					},
+				},
+			},
+			want: Resource{
+				"kind":       "Kustomization",
+				"apiVersion": "kustomize.config.k8s.io/v1beta1",
+				"resources":  []string{"deployment.yaml", "service.yaml"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Parser{
+				Resources: tt.fields.Resources,
+			}
+			if got := p.Kustomize(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Parser.Kustomize() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name string
+		want *Parser
+	}{
+		{
+			name: "new",
+			want: &Parser{
+				Resources: make([]Resource, 0),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := New(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("New() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
